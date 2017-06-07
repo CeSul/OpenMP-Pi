e1c5bb33f76f78f6ae8099555c8e8fffe6295291
Cesar Sul <csul@usc.edu>
2017-06-07 14:19:57 -0700

    Added input parameters

--- omp_pi.c
+++ omp_pi.c
@@ -1,21 +1,73 @@
+// Use OpenMP to split up pi calculation between multiple threads
+// Each thread does some portion of an integral
+/******************************************************************************/
+// Usage: omp_pi [options]
+//
+// Options:
+//  -n <num_steps> Sets the number of steps to use for integral (default = 1e4)
+/******************************************************************************/
 #include <omp.h>
 #include <stdio.h>
-static unsigned long num_steps = 5e11;
+#include <stdlib.h>
+#include<string.h>
+unsigned long num_steps;
 double step,step_sqr;
-//#define NUM_THREADS 4
-void main (){
+char * program_name;
+
+void usage(){
+   printf("Usage is %s [options]\n",program_name);
+   printf("Options:\n");
+   printf("-n <grid_pts>   Sets number of grid points to use (default=1e4)\n");
+   exit(8);
+}
+
+void set_args(int argc, char* argv[],unsigned long* num_steps){
+  int num_steps_IsSet=0;
+  int len;
+  // Parse arguments
+  while ((argc >1) && (argv[1][0] == '-')){
+// argv[1][1] is the option character
+    switch(argv[1][1]){
+      case 'n':
+        // set num_steps
+        len = strlen(argv[2]);
+        char* end_ptr = (argv[2]+len-1);
+        *num_steps=strtoul(argv[2],&end_ptr,10);
+        num_steps_IsSet=1;
+        break;
+      default:
+        printf("Bad option %s\n",argv[1]);
+        usage();
+        break;
+      }
+      argv+=2;
+      argc-=2;
+  }
+  // Set defaults if necessary
+  if(!num_steps_IsSet){
+    *num_steps=1e4;
+  }
+
+}
+
+
+
+
+void main (int argc, char* argv[]){
+   program_name = argv[0];
+
    unsigned long i;
-   //int report_count=10;
-   //int should_report;
    double sum[20];
    double pi;
    int id;
    int nthreads;
+
+   set_args(argc,argv,&num_steps);
+   printf("num_steps=%lu\n",num_steps );
    step = 1.0/(double) num_steps;
    step_sqr=step*step;
    #pragma omp parallel shared(sum) private(id)
    {
-// Moving x within the parallel region actually sped this up
       double x_sqr;
       id = omp_get_thread_num();
       sum[id] = 0.0;
@@ -25,16 +77,11 @@ void main (){
         printf("Number of threads = %d \n", nthreads);
       }
       printf("Thread %d starting...\n", id);
+
    #pragma omp for schedule(static) private(x_sqr)
          for (i=1; i < num_steps; i++){
-            //x = i *step;
             x_sqr=i*i*step_sqr;
-            //to_add = 4.0/(1.0+x*x);
-            //sum[id] += to_add;
             sum[id] += 4.0/(1.0+x_sqr);
-            //printf("Thread %d : i= %d | x= %.10f | to_add= %.10f  | sum[%d]= %.6f\n", id, i, x, to_add, id, sum[id]);
-            //printf("%d,%d,%.10f,%.10f,%.6f\n", id, i, x, to_add,sum[id]);
-         }
 
      printf("Thread %d done.\n", id);
 // Parallel region over
@@ -42,7 +89,6 @@ void main (){
      //printf("Number of threads = %d \n", nthreads);
      for(i=0, pi=3.0; i<nthreads ;i++){
       pi += sum[i];
-      //printf("pi=%.16f\n",pi);
      }
      pi*=step;
      printf("\npi=%.16f\n",pi);
